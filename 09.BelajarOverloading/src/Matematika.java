21726453430de118ef85ee826552503da29ae3c1
ariamanggala <110913340+ariamanggala@users.noreply.github.com>
2022-10-30 20:37:10 +0700

    Add files via upload

---
+++ 09.BelajarOverloading/src/Matematika.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class Matematika {
+    int tambah (int nilai1, int nilai2){
+    System.out.println("Tambah Pertama");
+    int hasil = nilai1 + nilai2;
+    return hasil;
+    }
+    
+    int tambah (int nilai1, int nilai2, int nilai3){
+    System.out.println("Tambah Kedua");
+    int hasil = nilai1 + nilai2 + nilai3;
+    return hasil;
+    }
+    
+    double tambah (double nilai1, double nilai2){
+    System.out.println("Tambah Ketiga");
+    double hasil = nilai1 + nilai2;
+    return hasil;
+    }
+    
+    double tambah (double nilai1, double nilai2, double nilai3){
+    System.out.println("Tambah keempat");
+    double hasil = nilai1 + nilai2 + nilai3;
+    return hasil;
+    }
+}
